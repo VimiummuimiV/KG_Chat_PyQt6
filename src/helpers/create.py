18cc25850095e7cb88be00c353e51fc4367a5e81
Aboba <blahblahmail.com>
2026-01-03 00:59:32 +0200

    create.py modified

--- src/helpers/create.py
+++ src/helpers/create.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from PyQt6.QtWidgets import QPushButton
 from PyQt6.QtGui import QIcon
-from PyQt6.QtCore import QSize
+from PyQt6.QtCore import QSize, Qt
 
 
 def create_icon_button(icons_path: Path, icon_name: str, tooltip: str = "", icon_size: int = 30, button_size: int = 48):
@@ -9,6 +9,9 @@ def create_icon_button(icons_path: Path, icon_name: str, tooltip: str = "", icon
     button.setIcon(QIcon(str(icons_path / icon_name)))
     button.setIconSize(QSize(icon_size, icon_size))
     button.setFixedSize(button_size, button_size)
+    button.setCursor(Qt.CursorShape.PointingHandCursor)
+    
     if tooltip:
         button.setToolTip(tooltip)
+    
     return button
