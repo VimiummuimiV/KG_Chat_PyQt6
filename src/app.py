e1f2a2e328459241198cb0a9adde65e08c52a4e6
Aboba <blahblahmail.com>
2026-01-02 16:54:56 +0200

    store config manager into helpers folder

--- src/app.py
+++ src/app.py
@@ -8,31 +8,7 @@ from PyQt6.QtWidgets import (
 from PyQt6.QtGui import QFont, QIcon
 from PyQt6.QtCore import Qt, QSize
 
-class Config:
-    def __init__(self, path):
-        self.path = path
-        self.data = self.load()
-    
-    def load(self):
-        with open(self.path, 'r') as f:
-            return json.load(f)
-    
-    def save(self):
-        with open(self.path, 'w') as f:
-            json.dump(self.data, f, indent=2)
-    
-    def get(self, *keys):
-        value = self.data
-        for key in keys:
-            value = value.get(key)
-        return value
-    
-    def set(self, *keys, value):
-        d = self.data
-        for key in keys[:-1]:
-            d = d.setdefault(key, {})
-        d[keys[-1]] = value
-        self.save()
+from helpers.config import Config
 
 
 class MainWindow(QWidget):
