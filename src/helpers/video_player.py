625e895796af3fc47d92afbc4686f98bb99cc829
Aboba <blahblahmail.com>
2026-09-01 19:53:46 +0300

    Added kick support in aliases.

--- src/helpers/video_player.py
+++ src/helpers/video_player.py
@@ -279,6 +279,7 @@ class VideoPlayer(QWidget):
             "youtu.be": "YouTube",
             "pikabu.ru": "Pikabu",
             "twitch.tv": "Twitch",
+            "kick.com": "Kick",
             "vk.ru": "VK",
             "vkvideo.ru": "VKVideo",
             "vimeo.com": "Vimeo",
